c8f8a599f65d530e8bc0f692973538a845da75dd
Erik Olsen <erawk26@gmail.com>
2018-03-31 17:27:15 -0400

    fixed logic and file saving

--- api/apartments.php
+++ api/apartments.php
@@ -1,39 +1,31 @@
 <?php
-if (isset($_GET)) {
+if(isset($_GET)) {
 	deliver_response(200, "success", apts());
 }
 function apts(){
 	$url = 'https://www.rentcafe.com/rentcafeapi.aspx?requestType=apartmentavailability&APIToken=NDY5OTI%3d-XDY6KCjhwhg%3d&propertyCode=p0155985';
-	$cacheFile = 'cache' . DIRECTORY_SEPARATOR . md5($url);
-	if (file_exists($cacheFile)) {
-		$fh = fopen($cacheFile, 'r');
-		$cacheTime = trim(fgets($fh));
-		if ((($cacheTime > strtotime('-1 minutes'))||(!get_new($url)))) {
-			return fread($fh, filesize($cacheFile));
-		}
-		fclose($fh);
-		unlink($cacheFile);
-	}
-	$json = get_new($url);
-	$fh = fopen($cacheFile, 'w');
-	fwrite($fh, time() . "\n");
-	fwrite($fh, $json);
-	fclose($fh);
-	return $json;
+	$cachefile = md5($url);
+	$timestamp=filemtime($cachefile);
+    $exists = file_exists($cachefile);
+    $over_time=(time() - $timestamp) > (10 * 60);
+    //fetch new data if file doesnt exist OR if time is up
+    //TODO: AND if service is working
+    if($over_time || !$exists){
+    file_put_contents($cachefile, json_encode(refresh_apts($url), LOCK_EX));
+    }
+	return json_decode(file_get_contents($cachefile));
 }
-
-function get_new($url){
+function refresh_apts($url) {
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_URL, $url);
-	$result = curl_exec($ch);
-	curl_close($ch);
-	$json = json_decode($result, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$result = curl_exec($ch);
+		curl_close($ch);
+	$json = json_decode($result,true);
 	return $json;
 }
-
-function deliver_response($status, $status_message, $data){
+function deliver_response($status, $status_message, $data) {
 	header("HTTP/1.1 $status $status_message");
 	$response['status'] = $status;
 	$response['status_message'] = $status_message;
--- js/index.js
+++ js/index.js
@@ -1,35 +1,49 @@
 var aptArr = [];
 $(document).ready(function () {
-    $.ajax({
+    $.ajax({//production
         type: "GET",
         url: "/api/apartments.php",
         dataType: "json",
         async: true,
         success: function (res) {
             aptArr = res['data'];
-            // console.log(res);
+             console.log(res);
         },
         error: function (res) {
             console.log('FAIL')
         }
-    }).then(function () {
-        if (aptArr.length > 0) {
-            aptList = '<ul class="apartments">';
-            aptArr.map(function (apt) {
-                var aptName = apt['ApartmentName'],
-                    numBeds = apt['Beds'],
-                    numBaths = apt['Baths'],
-                    floorName = apt['FloorplanName'],
-                    applyLink = apt['ApplyOnlineURL'];
-                aptList += "<li>" +
-                    "Name: " + aptName + "<br>" +
-                    "Floor plan: " + floorName + "<br>" +
-                    "Beds: " + numBeds + "<br>" +
-                    "Baths: " + numBaths + "<br>" +
-                    "<a href='" + applyLink + "'>Apply Online</a><br>" +
-                    "</li>";
-            });
-            $('#test-container').html(aptList + "</ul>");
-        }
-    });
+    })
+        .then(function () {
+            if (aptArr.length > 0) {
+                aptList = '<ul class="apartments">';
+                aptArr.map(function (apt) {
+                    var aptName = apt['ApartmentName'],
+                        numBeds = apt['Beds'],
+                        numBaths = apt['Baths'],
+                        floorName = apt['FloorplanName'],
+                        applyLink = apt['ApplyOnlineURL'];
+                    aptList += "<li>" +
+                        "Name: " + aptName + "<br>" +
+                        "Floor plan: " + floorName + "<br>" +
+                        "Beds: " + numBeds + "<br>" +
+                        "Baths: " + numBaths + "<br>" +
+                        "<a href='" + applyLink + "'>Apply Online</a><br>" +
+                        "</li>";
+                });
+                $('#test-container').html(aptList + "</ul>");
+            }
+        });
+    // $.ajax({//testing
+    //     type: "GET",
+    //     url: "/api/apartments.php",
+    //     dataType: "json",
+    //     async: true,
+    //     success: function (res) {
+    //         $('#test-container').html(res);
+    //         console.log(res['data']);
+    //     },
+    //     error: function (res) {
+    //         console.log('FAIL')
+    //     }
+    // });
 });
