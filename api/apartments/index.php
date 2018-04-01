cfc50bc5088f6b79ee3d8bdce3b3188bf899472a
Erik Olsen <erawk26@gmail.com>
2018-04-01 09:34:55 -0400

    all features working
    //TODO: add more styling
    //TODO: possibly change to CSS Grid for easier responsiveness
    //TODO: possibly change to vue for better templating

---
+++ api/apartments/index.php
@@ -0,0 +1,37 @@
+<?php
+function apts(){
+	$url = 'https://www.rentcafe.com/rentcafeapi.aspx?requestType=apartmentavailability&APIToken=NDY5OTI%3d-XDY6KCjhwhg%3d&propertyCode=p0155985';
+	$cachefile = md5($url).'.json';
+    $minutes = 10;
+	$timestamp=!file_exists($cachefile)?(time()-(($minutes+1) * 60)):filemtime($cachefile);//fileexists logic needs to happen before $overtime logic
+    $over_time=(time() - $timestamp) > ($minutes * 60);
+    if($over_time){
+        $data = refresh_apts($url,$cachefile);
+        file_put_contents($cachefile, json_encode($data, LOCK_EX));
+    }
+    $data = file_get_contents($cachefile);
+	return json_decode($data);
+}
+function refresh_apts($url,$file) {
+	$ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    $result = $httpCode == 404?file_get_contents($file):curl_exec($ch);
+	curl_close($ch);
+	$json = json_decode($result,true);
+	return $json;
+}
+function deliver_response($status, $status_message, $data) {
+	header("HTTP/1.1 $status $status_message");
+	$response['status'] = $status;
+	$response['status_message'] = $status_message;
+	$response['data'] = $data;
+	$json_response = json_encode($response);
+	echo $json_response;
+}
+if(isset($_GET)) {
+    $data=apts();
+    deliver_response(200, "success", $data);
+}
--- js/index.js
+++ js/index.js
@@ -2,32 +2,44 @@ var aptArr = [];
 $(document).ready(function () {
     $.ajax({//production
         type: "GET",
-        url: "/api/apartments.php",
+        url: "/api/apartments/",
         dataType: "json",
-        async: true,
+        async: false,
         success: function (res) {
             aptArr = res['data'];
              console.log(res);
         },
         error: function (res) {
-            console.log('FAIL')
+            console.log('FAIL');
+            console.log(res);
         }
     })
         .then(function () {
             if (aptArr.length > 0) {
-                aptList = '<ul class="apartments">';
+                aptList = "<ul class='apartments'>";
+                aptList += "<li>" +
+                    "<span class='label name'>Name</span>" +
+                    "<span class='label floor'>Floor plan</span>" +
+                    "<span class='label beds'>Beds</span>" +
+                    "<span class='label baths'>Baths</span>" +
+                    "<span class='label rent'>Rent</span>" +
+                    "<span class='label apply-link'></span>" +
+                    "</li>";
                 aptArr.map(function (apt) {
                     var aptName = apt['ApartmentName'],
                         numBeds = apt['Beds'],
-                        numBaths = apt['Baths'],
+                        numBaths = Math.round(apt['Baths']*2)/2,
                         floorName = apt['FloorplanName'],
+                        rentMin=parseInt(apt['MinimumRent']),
+                        rentMax=parseInt(apt['MaximumRent']),
                         applyLink = apt['ApplyOnlineURL'];
                     aptList += "<li>" +
-                        "Name: " + aptName + "<br>" +
-                        "Floor plan: " + floorName + "<br>" +
-                        "Beds: " + numBeds + "<br>" +
-                        "Baths: " + numBaths + "<br>" +
-                        "<a href='" + applyLink + "'>Apply Online</a><br>" +
+                        "<span class='name'>" + aptName + "</span>" +
+                        "<span class='floor'>" + floorName + "</span>" +
+                        "<span class='beds'>" + numBeds + "</span>" +
+                        "<span class='baths'>" + numBaths + "</span>" +
+                        "<span class='rent'>" + "\$"+rentMin+" - "+rentMax + "</span>" +
+                        "<a class='apply-link' target='_blank' href='" + applyLink + "'>Apply Online</a>" +
                         "</li>";
                 });
                 $('#test-container').html(aptList + "</ul>");
@@ -39,11 +51,12 @@ $(document).ready(function () {
     //     dataType: "json",
     //     async: true,
     //     success: function (res) {
-    //         $('#test-container').html(res);
+    //         $('#test-container').html(res['data']);
     //         console.log(res['data']);
     //     },
     //     error: function (res) {
-    //         console.log('FAIL')
+    //         $('#test-container').html(res.responseText);
+    //         console.log('FAIL\n');
     //     }
     // });
 });
